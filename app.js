6f37f495c4745044f42b191f9351cc2bacf26c36
LEEKY <ygy5036@gmail.com>
2019-09-02 22:33:20 +0900

    ver0.3

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
 
+canvas.width = 500;
+canvas.height = 500;
+
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
@@ -16,9 +19,11 @@ const onMouseMove = (event) => {
     const x = event.offsetX;
     const y = event.offsetY;
     if (!painting) {
+        console.log('Createing in', x, y)
         ctx.beginPath();
         ctx.moveTo(x, y);
     } else {
+        console.log('Createing', x, y)
         ctx.lineTo(x, y);
         ctx.stroke();
     }
